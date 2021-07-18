efb390aa4586042c9847587fb1a1a418e0eccc2e
Jonathan Dahan <git@jonathan.is>
2021-07-17 20:18:55 -0400

    Make idempotent, use working image

--- base.sh
+++ base.sh
@@ -1,27 +1,34 @@
 #!/bin/sh -e
 
-kiss_container=$(buildah from scratch)
-kiss_mount=$(buildah mount $kiss_container)
-kiss_chroot_uri=https://github.com/kisslinux/repo/releases/download/2021.7-6
-kiss_chroot_file=kiss-chroot-2021.7-6.tar.xz
-curl --location --silent --remote-name $kiss_chroot_uri/$kiss_chroot_file
-curl --location --silent $kiss_chroot_uri/$kiss_chroot_file.sha256 | sha256sum -c
-echo TODO: key verification
-cp $kiss_chroot_file $kiss_mount/
-tar x -C $kiss_mount -f $kiss_chroot_file
-buildah commit $kiss_container kiss
-buildah tag localhost/kiss 2021.7.6
-buildah unmount $kiss_container
 
-kiss_container=$(buildah from localhost/kiss)
-kiss_mount=$(buildah mount $kiss_container)
-echo running for $kiss_container
-buildah run $kiss_container git clone https://github.com/kisslinux/repo /repos/official
-buildah config \
-  --env KISS_PATH=/repos/official/core:/repos/official/extra:/repos/offical/wayland \
-  --env KISS_PROMPT=0 \
-  $kiss_container
-echo ERROR will happen in following command
-buildah run $kiss_container kiss update
-echo TODO: gpg verification
-buildah run $kiss_container bash
+buildah images localhost/kiss >/dev/null || {
+  kiss_version=2021.7-3
+  kiss_container=$(buildah from scratch)
+  kiss_mount=$(buildah mount $kiss_container)
+  kiss_chroot_uri=https://github.com/kisslinux/repo/releases/download/$kiss_version
+  kiss_chroot_file=kiss-chroot-$kiss_version.tar.xz
+  curl --location --silent --remote-name $kiss_chroot_uri/$kiss_chroot_file
+  curl --location --silent $kiss_chroot_uri/$kiss_chroot_file.sha256 | sha256sum -c
+  # TODO: key verification
+  cp $kiss_chroot_file $kiss_mount/
+  tar x -C $kiss_mount -f $kiss_chroot_file
+  buildah commit $kiss_container kiss
+  buildah tag localhost/kiss $kiss_version
+  buildah unmount $kiss_container
+}
+
+buildah images localhost/kiss-5.5.6 >/dev/null || {
+  kiss_container=$(buildah from localhost/kiss)
+  kiss_mount=$(buildah mount $kiss_container)
+  buildah run $kiss_container git clone https://github.com/kisslinux/repo /repos/official
+  buildah config \
+    --env KISS_PATH=/repos/official/core:/repos/official/extra:/repos/offical/wayland \
+    --env KISS_PROMPT=0 \
+    $kiss_container
+  buildah run $kiss_container kiss update
+  # TODO: gpg verification
+  kiss_binary_version=$(buildah run $kiss_container kiss version)
+  buildah commit $kiss_container localhost/kiss-$kiss_binary_version
+}
+
+echo podman run localhost/kiss-5.5.6 kiss list
